0d79675693e9c4a97e549e25c9118e31041f8e4b
Affaxed <delhinrharl@gmail.com>
2021-11-15 11:02:13 +0000

    modified html/css/script to show menu

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
+const main = document.getElementById("navMenu");
+
+document.getElementById("menu").addEventListener("click", () => {
+    main.style.display = "block";
+});
+
 function openNavlinks() {
-  document.getElementById('navMenu').style.width = '100%';
-  document.getElementById('navxid').style.marginLeft = '250px';
+    document.getElementById("navMenu").style.width = "100%";
+    document.getElementById("navxid").style.marginLeft = "250px";
 }
 
-openNavlinks();
-
 function closeNavMenu() {
-  document.getElementById('navMenu').style.width = '0';
-  document.getElementById('navxid').style.marginLeft = '0';
+    document.getElementById("navMenu").style.width = "0";
+    document.getElementById("navxid").style.marginLeft = "0";
 }
 
 closeNavMenu();
 
 function hidenavblock() {
-  const x = document.getElementById('navMenu');
-  if (x.style.display === 'none') {
-    x.style.display = 'block';
-  } else {
-    x.style.display = 'none';
-  }
+    const x = document.getElementById("navMenu");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
 }
 
 hidenavblock();
