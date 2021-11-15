ec087543da2ee08d65659e5ff0c4bd64c8ef580f
Azonkeu <mazonkeu@gmail.com>
2021-11-15 11:34:57 +0100

    Fix linters issues

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 function openNavlinks() {
-    document.getElementById("navMenu").style.width = "100%";
-    document.getElementById("navxid").style.marginLeft = "250px";
+  document.getElementById('navMenu').style.width = '100%';
+  document.getElementById('navxid').style.marginLeft = '250px';
 }
+
+openNavlinks();
 
 function closeNavMenu() {
-    document.getElementById("navMenu").style.width = "0";
-    document.getElementById("navxid").style.marginLeft = "0";
+  document.getElementById('navMenu').style.width = '0';
+  document.getElementById('navxid').style.marginLeft = '0';
 }
 
+closeNavMenu();
+
 function hidenavblock() {
-    var x = document.getElementById("navMenu");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
+  const x = document.getElementById('navMenu');
+  if (x.style.display === 'none') {
+    x.style.display = 'block';
+  } else {
+    x.style.display = 'none';
   }
+}
+
+hidenavblock();
