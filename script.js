5f2a08bdb723c8ae159c4c0d9233c6e1778526f7
Azonkeu <mazonkeu@gmail.com>
2021-11-15 19:01:43 +0100

    Fix linters issues

--- script.js
+++ script.js
@@ -9,19 +9,19 @@ closeNavBlock.addEventListener('click', () => {
 });
 
 const openNavlink = document.getElementById('portfolio');
-openNavlink.addEventListener('click', () => { 
+openNavlink.addEventListener('click', () => {
   document.getElementById('navMenu').style.display = 'none';
   window.location.reload();
 });
 
 const openlinkAbout = document.getElementById('about');
-openlinkAbout.addEventListener('click', () => { 
+openlinkAbout.addEventListener('click', () => {
   document.getElementById('navMenu').style.display = 'none';
   window.location.reload();
 });
 
 const openlinkContact = document.getElementById('contact');
-openlinkContact.addEventListener('click', () => { 
+openlinkContact.addEventListener('click', () => {
   document.getElementById('navMenu').style.display = 'none';
   window.location.reload();
 });
