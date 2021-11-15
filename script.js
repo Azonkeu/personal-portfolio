28e108550424894b520cedca62fa9941b0eb5071
Azonkeu <mazonkeu@gmail.com>
2021-11-15 14:39:32 +0100

    Fix linter issues

--- script.js
+++ script.js
@@ -1,28 +1,21 @@
-const main = document.getElementById("navMenu");
-
-document.getElementById("menu").addEventListener("click", () => {
-    main.style.display = "block";
+const element = document.getElementById('hamNenu');
+element.addEventListener('click', () => {
+  document.getElementById('navMenu').style.width = '100%';
 });
 
-function openNavlinks() {
-    document.getElementById("navMenu").style.width = "100%";
-    document.getElementById("navxid").style.marginLeft = "250px";
-}
-
 function closeNavMenu() {
-    document.getElementById("navMenu").style.width = "0";
-    document.getElementById("navxid").style.marginLeft = "0";
+  document.getElementById('navMenu').style.width = '0';
+  document.getElementById('navxid').style.marginLeft = '0';
 }
-
 closeNavMenu();
 
 function hidenavblock() {
-    const x = document.getElementById("navMenu");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
+  const x = document.getElementById('navMenu');
+  if (x.style.display === 'none') {
+    x.style.display = 'block';
+  } else {
+    x.style.display = 'none';
+  }
+  window.location.reload();
 }
-
 hidenavblock();
